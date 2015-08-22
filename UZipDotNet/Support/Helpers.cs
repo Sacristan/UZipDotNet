0717bb0fc536b55d10739399708455c7da4a6277
Sacristan <girts.kesteris@gmail.com>
2015-08-22 19:56:50 +0300

    Added Support Helpers class with Compress and Decompress Directory Logic

---
+++ UZipDotNet/Support/Helpers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace UZipDotNet.Support
+{
+    public class Helpers
+    {
+        public static void CompressDirectory(string directoryPath, string archivePath, string archiveName, bool deleteDirectory=false)
+        {
+            string archivefile = Path.Combine(archivePath, archiveName);
+            Directory.CreateDirectory(archivePath);
+
+            string[] files = Directory.GetFiles(directoryPath);
+
+            using (var def = new UZipDotNet.DeflateZipFile(@archivefile))
+            {
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    def.Compress(file, fileName);
+                }
+                def.Save();
+            }
+
+            if(deleteDirectory)
+                Directory.Delete(directoryPath, true);
+        }
+
+        public static void DecompressDirectory(string filePath, string decompressPath)
+        {
+            using(var inf = new UZipDotNet.InflateZipFile(@filePath))
+            {
+                inf.DecompressAll(decompressPath, true);
+            }
+        }
+
+    }
+}
